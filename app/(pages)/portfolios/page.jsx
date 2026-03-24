f9bae89f3f6ac2273a8b8e30974710ab18f0215f
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2026-03-24 12:15:32 +0530

    updated navbar

---
+++ app/(pages)/portfolios/page.jsx
@@ -0,0 +1,8 @@
+import PortfolioSection from '@/app/components/PortfolioSection'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div><PortfolioSection /></div>
+  )
+}
